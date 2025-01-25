064a3b80db48f7bec9a2ba225cea2f4123b5cf88
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2025-01-25 19:03:59 +0500

    feat: add stream filters

--- server/src/modules/stream/stream.resolver.ts
+++ server/src/modules/stream/stream.resolver.ts
@@ -1,13 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { StreamService } from './stream.service';
 import { StreamModel } from './models/stream.model';
+import { FiltersInput } from './inputs/filters.input';
 
 @Resolver('Stream')
 export class StreamResolver {
   constructor(private readonly streamService: StreamService) { }
 
   @Query(() => [StreamModel], { name: "findAllStreams" })
-  public async findAll() {
-    return this.streamService.findAll()
+  public async findAll(@Args('filters') input: FiltersInput) {
+    return this.streamService.findAll(input)
   }
 }
--- server/src/modules/stream/stream.service.ts
+++ server/src/modules/stream/stream.service.ts
@@ -1,12 +1,23 @@
 import { PrismaService } from '@/src/core/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { FiltersInput } from './inputs/filters.input';
+import { Prisma } from '@prisma/generated';
 
 @Injectable()
 export class StreamService {
     public constructor(private readonly prismaService: PrismaService) { }
 
-    public async findAll() {
+    public async findAll(input: FiltersInput = {}) {
+        const { take, skip, searchKey } = input
+
+        const whereClause = searchKey ? this.findBySearchKeyFilter(searchKey) : undefined
+
         const streams = await this.prismaService.stream.findMany({
+            take: take ?? 15,
+            skip: skip ?? 0,
+            where: {
+                ...whereClause
+            },
             include: {
                 user: true
             }
@@ -15,4 +26,24 @@ export class StreamService {
         return streams
     }
 
+    private findBySearchKeyFilter(searchKey: string): Prisma.StreamWhereInput {
+        return {
+            OR: [
+                {
+                    title: {
+                        contains: searchKey,
+                        mode: 'insensitive'
+                    }
+                },
+                {
+                    user: {
+                        username: {
+                            contains: searchKey,
+                            mode: 'insensitive'
+                        }
+                    }
+                }
+            ]
+        }
+    }
 }
