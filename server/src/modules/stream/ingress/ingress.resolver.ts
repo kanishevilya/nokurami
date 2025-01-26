a6e9807a792f5ff7e849c1053e3846ccea574bd4
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2025-01-26 12:39:21 +0500

    feat: ingress module

--- server/src/modules/stream/ingress/ingress.resolver.ts
+++ server/src/modules/stream/ingress/ingress.resolver.ts
@@ -1,7 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { IngressService } from './ingress.service';
+import { Authorization } from '@/src/shared/decorators/auth.decorator';
+import { Authorized } from '@/src/shared/decorators/authorized.decorator';
+import { User } from '@/prisma/generated';
+import { IngressInput } from 'livekit-server-sdk';
 
 @Resolver('Ingress')
 export class IngressResolver {
-  constructor(private readonly ingressService: IngressService) {}
+  constructor(private readonly ingressService: IngressService) { }
+
+  @Authorization()
+  @Mutation(() => Boolean, { name: 'createIngress' })
+  public async create(
+    @Authorized() user: User,
+    @Args('ingressType') ingressType: IngressInput
+  ) {
+    return this.ingressService.create(user, ingressType)
+  }
 }
