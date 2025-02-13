63bd77704e5b4a18137a35dcf7023fe7f4d2f113
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2025-02-13 14:48:02 +0500

    feat: web configuration

---
+++ web/src/graphql/generated/output.ts
@@ -0,0 +1,558 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
+  Upload: { input: any; output: any; }
+};
+
+export type CategoryModel = {
+  __typename?: 'CategoryModel';
+  createdAt: Scalars['DateTime']['output'];
+  description: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  previewUrl: Scalars['String']['output'];
+  slug: Scalars['String']['output'];
+  streams: Array<StreamModel>;
+  title: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+export type ChangeChatSettingsInput = {
+  isChatEnabled: Scalars['Boolean']['input'];
+  isChatFollowersOnly: Scalars['Boolean']['input'];
+  isChatSubscribersOnly: Scalars['Boolean']['input'];
+};
+
+export type ChangeEmailInput = {
+  email: Scalars['String']['input'];
+  token: Scalars['String']['input'];
+};
+
+export type ChangeNotificationsSettingsInput = {
+  siteNotificationsEnable: Scalars['Boolean']['input'];
+  telegramNotificationsEnable: Scalars['Boolean']['input'];
+};
+
+export type ChangeNotificationsSettingsResponse = {
+  __typename?: 'ChangeNotificationsSettingsResponse';
+  notificationSettings: NotificationSettingsModel;
+  telegramToken?: Maybe<Scalars['String']['output']>;
+};
+
+export type ChangePasswordInput = {
+  newPassword: Scalars['String']['input'];
+  oldPassword: Scalars['String']['input'];
+};
+
+export type ChangeProfileInfoInput = {
+  displayName: Scalars['String']['input'];
+  information: Scalars['String']['input'];
+  username: Scalars['String']['input'];
+};
+
+export type ChangeStreamInfoInput = {
+  categoryId: Scalars['String']['input'];
+  title: Scalars['String']['input'];
+};
+
+export type CreateUserInput = {
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+  username: Scalars['String']['input'];
+};
+
+export type DeviceModel = {
+  __typename?: 'DeviceModel';
+  browser: Scalars['String']['output'];
+  os: Scalars['String']['output'];
+  type: Scalars['String']['output'];
+};
+
+export type Enable2FaInput = {
+  pin: Scalars['String']['input'];
+  secret: Scalars['String']['input'];
+};
+
+export type FiltersInput = {
+  searchKey?: InputMaybe<Scalars['String']['input']>;
+  skip?: InputMaybe<Scalars['Float']['input']>;
+  take?: InputMaybe<Scalars['Float']['input']>;
+};
+
+export type FollowModel = {
+  __typename?: 'FollowModel';
+  createdAt: Scalars['DateTime']['output'];
+  follower: UserModel;
+  followerId: Scalars['String']['output'];
+  following: UserModel;
+  followingId: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+export type GenerateStreamTokenInput = {
+  channelId: Scalars['String']['input'];
+  userId: Scalars['String']['input'];
+};
+
+export type GenerateStreamTokenModel = {
+  __typename?: 'GenerateStreamTokenModel';
+  token: Scalars['String']['output'];
+};
+
+export type LocationModel = {
+  __typename?: 'LocationModel';
+  city: Scalars['String']['output'];
+  country: Scalars['String']['output'];
+  latitude: Scalars['String']['output'];
+  longitude: Scalars['String']['output'];
+};
+
+export type LoginInput = {
+  login: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+  pin?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type MessageModel = {
+  __typename?: 'MessageModel';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  stream: StreamModel;
+  streamId: Scalars['String']['output'];
+  text: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+  user: UserModel;
+  userId: Scalars['String']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  changeChatSettings: Scalars['Boolean']['output'];
+  changeEmail: Scalars['Boolean']['output'];
+  changeNotificationSettings: ChangeNotificationsSettingsResponse;
+  changePassword: Scalars['Boolean']['output'];
+  changeProfileAvatar: Scalars['Boolean']['output'];
+  changeProfileInfo: Scalars['Boolean']['output'];
+  changeStreamInfo: Scalars['Boolean']['output'];
+  changeStreamPreview: Scalars['Boolean']['output'];
+  clearSessionFromCookie: Scalars['Boolean']['output'];
+  createIngress: Scalars['Boolean']['output'];
+  createSocialLink: Scalars['Boolean']['output'];
+  createUser: Scalars['Boolean']['output'];
+  disable2FA: Scalars['Boolean']['output'];
+  enable2FA: Scalars['Boolean']['output'];
+  followToChannel: Scalars['Boolean']['output'];
+  generateStreamToken: GenerateStreamTokenModel;
+  login: UserModel;
+  logout: Scalars['Boolean']['output'];
+  new_password: Scalars['Boolean']['output'];
+  removeProfileAvatar: Scalars['Boolean']['output'];
+  removeSession: Scalars['Boolean']['output'];
+  removeSocialLink: Scalars['Boolean']['output'];
+  removeStreamPreview: Scalars['Boolean']['output'];
+  reorderSocialLinks: Scalars['Boolean']['output'];
+  requestToEmailChange: Scalars['Boolean']['output'];
+  reset_password: Scalars['Boolean']['output'];
+  sendMessage: MessageModel;
+  unfollowFromChannel: Scalars['Boolean']['output'];
+  updateSocialLink: Scalars['Boolean']['output'];
+  verifyAccount: UserModel;
+};
+
+
+export type MutationChangeChatSettingsArgs = {
+  data: ChangeChatSettingsInput;
+};
+
+
+export type MutationChangeEmailArgs = {
+  data: ChangeEmailInput;
+};
+
+
+export type MutationChangeNotificationSettingsArgs = {
+  data: ChangeNotificationsSettingsInput;
+};
+
+
+export type MutationChangePasswordArgs = {
+  data: ChangePasswordInput;
+};
+
+
+export type MutationChangeProfileAvatarArgs = {
+  avatar: Scalars['Upload']['input'];
+};
+
+
+export type MutationChangeProfileInfoArgs = {
+  data: ChangeProfileInfoInput;
+};
+
+
+export type MutationChangeStreamInfoArgs = {
+  data: ChangeStreamInfoInput;
+};
+
+
+export type MutationChangeStreamPreviewArgs = {
+  thumbnail: Scalars['Upload']['input'];
+};
+
+
+export type MutationCreateIngressArgs = {
+  ingressType: Scalars['Float']['input'];
+};
+
+
+export type MutationCreateSocialLinkArgs = {
+  data: SocialLinkInput;
+};
+
+
+export type MutationCreateUserArgs = {
+  data: CreateUserInput;
+};
+
+
+export type MutationEnable2FaArgs = {
+  data: Enable2FaInput;
+};
+
+
+export type MutationFollowToChannelArgs = {
+  channelId: Scalars['String']['input'];
+};
+
+
+export type MutationGenerateStreamTokenArgs = {
+  data: GenerateStreamTokenInput;
+};
+
+
+export type MutationLoginArgs = {
+  data: LoginInput;
+};
+
+
+export type MutationNew_PasswordArgs = {
+  data: NewPasswordInput;
+};
+
+
+export type MutationRemoveSessionArgs = {
+  id: Scalars['String']['input'];
+};
+
+
+export type MutationRemoveSocialLinkArgs = {
+  id: Scalars['String']['input'];
+};
+
+
+export type MutationReorderSocialLinksArgs = {
+  list: Array<SocialLinkOrderInput>;
+};
+
+
+export type MutationReset_PasswordArgs = {
+  data: ResetPasswordInput;
+};
+
+
+export type MutationSendMessageArgs = {
+  data: SendMessageInput;
+};
+
+
+export type MutationUnfollowFromChannelArgs = {
+  channelId: Scalars['String']['input'];
+};
+
+
+export type MutationUpdateSocialLinkArgs = {
+  data: SocialLinkInput;
+  id: Scalars['String']['input'];
+};
+
+
+export type MutationVerifyAccountArgs = {
+  data: VerificationInput;
+};
+
+export type NewPasswordInput = {
+  confirmPassword: Scalars['String']['input'];
+  newPassword: Scalars['String']['input'];
+  token: Scalars['String']['input'];
+};
+
+export type NotificationModel = {
+  __typename?: 'NotificationModel';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['String']['output'];
+  isRead: Scalars['Boolean']['output'];
+  message: Scalars['String']['output'];
+  type: NotificationType;
+  updatedAt: Scalars['DateTime']['output'];
+  user: UserModel;
+  userId: Scalars['String']['output'];
+};
+
+export type NotificationSettingsModel = {
+  __typename?: 'NotificationSettingsModel';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['String']['output'];
+  siteNotificationsEnable: Scalars['Boolean']['output'];
+  telegramNotificationsEnable: Scalars['Boolean']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+  user: UserModel;
+  userId: Scalars['String']['output'];
+};
+
+export enum NotificationType {
+  EnableTwoFactor = 'ENABLE_TWO_FACTOR',
+  NewFollower = 'NEW_FOLLOWER',
+  StreamStart = 'STREAM_START'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  findAllCategories: Array<CategoryModel>;
+  findAllStreams: Array<StreamModel>;
+  findCategoryBySlug: CategoryModel;
+  findChannelByUsername: UserModel;
+  findFollowersCountByChannel: Scalars['Float']['output'];
+  findMessagesByStream: Array<MessageModel>;
+  findMyFollowers: Array<FollowModel>;
+  findMyFollowings: Array<FollowModel>;
+  findNotificationsByUserId: Array<NotificationModel>;
+  findRandomCategories: Array<CategoryModel>;
+  findRandomStreams: Array<StreamModel>;
+  findRecommendedChannels: Array<UserModel>;
+  findSocialLinks: Array<SocialLinkModel>;
+  findUnreadNotificationsCount: Scalars['Float']['output'];
+  generateTotpSecret: TotpModel;
+  getAllSessions: Array<SessionModel>;
+  getCurrentSession: SessionModel;
+  getProfile: UserModel;
+  getSessionsByUser: Array<SessionModel>;
+};
+
+
+export type QueryFindAllStreamsArgs = {
+  filters: FiltersInput;
+};
+
+
+export type QueryFindCategoryBySlugArgs = {
+  slug: Scalars['String']['input'];
+};
+
+
+export type QueryFindChannelByUsernameArgs = {
+  username: Scalars['String']['input'];
+};
+
+
+export type QueryFindFollowersCountByChannelArgs = {
+  channelId: Scalars['String']['input'];
+};
+
+
+export type QueryFindMessagesByStreamArgs = {
+  streamId: Scalars['String']['input'];
+};
+
+export type ResetPasswordInput = {
+  email: Scalars['String']['input'];
+};
+
+export type SendMessageInput = {
+  streamId: Scalars['String']['input'];
+  text: Scalars['String']['input'];
+};
+
+export type SessionMetadataModel = {
+  __typename?: 'SessionMetadataModel';
+  device: DeviceModel;
+  ip: Scalars['String']['output'];
+  location: LocationModel;
+};
+
+export type SessionModel = {
+  __typename?: 'SessionModel';
+  createdAt: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  metadata: SessionMetadataModel;
+  userId: Scalars['String']['output'];
+};
+
+export type SocialLinkInput = {
+  title: Scalars['String']['input'];
+  url: Scalars['String']['input'];
+};
+
+export type SocialLinkModel = {
+  __typename?: 'SocialLinkModel';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  position: Scalars['Float']['output'];
+  title: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+  url: Scalars['String']['output'];
+  userId: Scalars['String']['output'];
+};
+
+export type SocialLinkOrderInput = {
+  id: Scalars['String']['input'];
+  position: Scalars['Float']['input'];
+};
+
+export type StreamModel = {
+  __typename?: 'StreamModel';
+  category: CategoryModel;
+  categoryId: Scalars['String']['output'];
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  ingressId?: Maybe<Scalars['String']['output']>;
+  isChatEnabled: Scalars['Boolean']['output'];
+  isChatFollowersOnly: Scalars['Boolean']['output'];
+  isChatSubscribersOnly: Scalars['Boolean']['output'];
+  isLive: Scalars['Boolean']['output'];
+  previewUrl?: Maybe<Scalars['String']['output']>;
+  serverUrl?: Maybe<Scalars['String']['output']>;
+  streamKey?: Maybe<Scalars['String']['output']>;
+  title: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+  user: UserModel;
+  userId: Scalars['String']['output'];
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  newMessageAdded: MessageModel;
+  newNotificationAdded: NotificationModel;
+};
+
+
+export type SubscriptionNewMessageAddedArgs = {
+  streamId: Scalars['String']['input'];
+};
+
+
+export type SubscriptionNewNotificationAddedArgs = {
+  userId: Scalars['String']['input'];
+};
+
+export type TotpModel = {
+  __typename?: 'TotpModel';
+  qrcodeUrl: Scalars['String']['output'];
+  secret: Scalars['String']['output'];
+};
+
+export type UserModel = {
+  __typename?: 'UserModel';
+  avatar?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['DateTime']['output'];
+  displayName: Scalars['String']['output'];
+  email: Scalars['String']['output'];
+  followers: Array<FollowModel>;
+  followings: Array<FollowModel>;
+  id: Scalars['ID']['output'];
+  information?: Maybe<Scalars['String']['output']>;
+  notificationSettings: NotificationSettingsModel;
+  notifications: Array<NotificationModel>;
+  password: Scalars['String']['output'];
+  role: UserRole;
+  socialLinks: Array<SocialLinkModel>;
+  stream: StreamModel;
+  telegramChatId?: Maybe<Scalars['String']['output']>;
+  updatedAt: Scalars['DateTime']['output'];
+  userSecurity: UserSecurityModel;
+  username: Scalars['String']['output'];
+};
+
+export enum UserRole {
+  Admin = 'ADMIN',
+  Regular = 'REGULAR'
+}
+
+export type UserSecurityModel = {
+  __typename?: 'UserSecurityModel';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  isEmailVerified: Scalars['Boolean']['output'];
+  isTwoFAEnabled: Scalars['Boolean']['output'];
+  twoFASecret: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+  userId: Scalars['String']['output'];
+};
+
+export type VerificationInput = {
+  token: Scalars['String']['input'];
+};
+
+export type FindChannelByUsernameQueryVariables = Exact<{
+  username: Scalars['String']['input'];
+}>;
+
+
+export type FindChannelByUsernameQuery = { __typename?: 'Query', findChannelByUsername: { __typename?: 'UserModel', username: string, avatar?: string | null } };
+
+
+export const FindChannelByUsernameDocument = gql`
+    query FindChannelByUsername($username: String!) {
+  findChannelByUsername(username: $username) {
+    username
+    avatar
+  }
+}
+    `;
+
+/**
+ * __useFindChannelByUsernameQuery__
+ *
+ * To run a query within a React component, call `useFindChannelByUsernameQuery` and pass it any options that fit your needs.
+ * When your component renders, `useFindChannelByUsernameQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useFindChannelByUsernameQuery({
+ *   variables: {
+ *      username: // value for 'username'
+ *   },
+ * });
+ */
+export function useFindChannelByUsernameQuery(baseOptions: Apollo.QueryHookOptions<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables> & ({ variables: FindChannelByUsernameQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>(FindChannelByUsernameDocument, options);
+      }
+export function useFindChannelByUsernameLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>(FindChannelByUsernameDocument, options);
+        }
+export function useFindChannelByUsernameSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>(FindChannelByUsernameDocument, options);
+        }
+export type FindChannelByUsernameQueryHookResult = ReturnType<typeof useFindChannelByUsernameQuery>;
+export type FindChannelByUsernameLazyQueryHookResult = ReturnType<typeof useFindChannelByUsernameLazyQuery>;
+export type FindChannelByUsernameSuspenseQueryHookResult = ReturnType<typeof useFindChannelByUsernameSuspenseQuery>;
+export type FindChannelByUsernameQueryResult = Apollo.QueryResult<FindChannelByUsernameQuery, FindChannelByUsernameQueryVariables>;
