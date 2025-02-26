84a7e22996941c9b78d8e01ca6972b4bdf8b5672
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2025-02-26 23:00:42 +0500

    fix: bug fix & dashboard setting complete

--- web/src/components/ui/shadcn/Accordion.tsx
+++ web/src/components/ui/shadcn/Accordion.tsx
@@ -26,6 +26,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
+        alwaysOpen ? "cursor-default" : "",
         "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180",
         className
       )}
